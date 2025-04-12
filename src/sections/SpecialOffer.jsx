be99937936005aa5f529d66da78a21f3becda329
MohamedHany17m8 <113649379+MohamedHany17m8@users.noreply.github.com>
2025-04-12 23:36:17 +0200

    Update SpecialOffer.jsx
    women

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,18 +1,12 @@
 import { arrowRight } from "../assets/icons";
-import { offer } from "../assets/images";
+
 import { Button } from "../components";
 
 const SpecialOffer = () => {
   return (
     <section className='flex justify-between items-center max-xl:flex-col-reverse gap-10 max-container'>
       <div className='flex-1'>
-        <img
-          src={offer}
-          alt='Shoe Promotion'
-          width={773}
-          height={687}
-          className='object-contain w-full'
-        />
+    
       </div>
       <div className='flex flex-1 flex-col'>
         <h2 className='text-4xl font-palanquin font-bold'>
