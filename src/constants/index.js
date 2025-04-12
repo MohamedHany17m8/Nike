36f90ddee6fedff831817ccd3bff3b363d94f629
MohamedHany17m8 <113649379+MohamedHany17m8@users.noreply.github.com>
2025-04-12 23:38:42 +0200

    Update index.js
    lota

--- src/constants/index.js
+++ src/constants/index.js
@@ -77,12 +77,7 @@ export const reviews = [
         rating: 4.5,
         feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
     },
-    {
-        imgURL: customer2,
-        customerName: 'Lota Mongeskar',
-        rating: 4.5,
-        feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
-    }
+    
 ];
 
 
